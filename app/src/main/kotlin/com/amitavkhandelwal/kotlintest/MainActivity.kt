ab10c1f33facda88eccf0e732300c0b2b197c13c
Khandelwal, Amitav <amitav.khandelwal@gmail.com>
2015-09-11 15:54:50 -0400

    Initial commit

---
+++ app/src/main/kotlin/com/amitavkhandelwal/kotlintest/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.amitavkhandelwal.kotlintest
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.activity_main.*
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        Since we're using kotlin extension for Android, we don't need the below statement
+//        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+//        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
+            }
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.getItemId()
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+}
